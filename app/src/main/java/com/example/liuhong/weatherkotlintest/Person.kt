0a2b3a42bd6532187f06144b4e850d01369bdd13
duhongliu <duhongliu@mafengwo.com>
2018-03-05 14:02:05 +0800

    如何定义一个类

---
+++ app/src/main/java/com/example/liuhong/weatherkotlintest/Person.kt
@@ -0,0 +1,13 @@
+package com.example.liuhong.weatherkotlintest
+
+/***
+ * Created by liuhong on 2018/3/5.
+ * Description: Person类
+ */
+class Person(name: String, surname: String) {
+
+    init {
+        //构造函数的函数体
+    }
+
+}
