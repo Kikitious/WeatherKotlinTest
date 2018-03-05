4005539924615f7a4d8b5ec7b56ed835319f2ef3
duhongliu <duhongliu@mafengwo.com>
2018-03-05 14:26:50 +0800

    如何定义一个类

--- app/src/main/java/com/example/liuhong/weatherkotlintest/MainActivity.kt
+++ app/src/main/java/com/example/liuhong/weatherkotlintest/MainActivity.kt
@@ -11,5 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         message.text = getString(R.string.weather_welcome_string)
 
+        add(3, 5)
+        addDirectly(3, 5)
     }
+
+    fun log(name: String): Unit {
+        //如果不指定返回值，默认返回Unit，类似于Java中的void
+        //但是Unit是一个真正的对象
+    }
+
+    fun add(x: Int, y: Int): Int {
+        return x + y
+    }
+
+    fun addDirectly(x: Int, y: Int): Int = x + y
+    //返回的结果可以使用一个表达式计算出来的，可以不使用括号直接使用等号
 }
