497f1d603b140a13f8fc387b07901894374b01d6
duhongliu <duhongliu@mafengwo.com>
2018-03-05 16:40:43 +0800

    The Recycler Adapter

---
+++ app/src/main/java/com/example/liuhong/weatherkotlintest/ForecastListAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.liuhong.weatherkotlintest
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/***
+ * Created by liuhong on 2018/3/5.
+ * Description: 天气预报ListAdapter
+ */
+class ForecastListAdapter(val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(textView = TextView(parent?.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder!!.textView.text = items.get(position)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+}
--- app/src/main/java/com/example/liuhong/weatherkotlintest/MainActivity.kt
+++ app/src/main/java/com/example/liuhong/weatherkotlintest/MainActivity.kt
@@ -8,11 +8,22 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private val items = listOf<String>(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Sunny - 31/18",
+            "Wed 6/25 - Sunny - 31/19",
+            "Thurs 6/26 - Sunny - 29/17",
+            "Fri 6/27 - Sunny - 31/27",
+            "Sat 6/28 - Sunny - 31/18",
+            "Sun 6/29 - Sunny - 31/14"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 
     fun niceToast(message: String,
