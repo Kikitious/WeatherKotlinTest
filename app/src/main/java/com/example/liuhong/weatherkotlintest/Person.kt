655908fd5ddcb9a14fa21c8f1b2872f5fe4eb32b
duhongliu <duhongliu@mafengwo.com>
2018-03-05 17:20:31 +0800

    属性

--- app/src/main/java/com/example/liuhong/weatherkotlintest/Person.kt
+++ app/src/main/java/com/example/liuhong/weatherkotlintest/Person.kt
@@ -7,6 +7,19 @@ package com.example.liuhong.weatherkotlintest
 class Person(name: String, surname: String) : Animal(name) {
     //当只有单个构造器时，需要在从父类继承下来的构造器中指定需要的参数，即java中的super
 
+    var age: Int = 18
+
+    var height: Int = 160
+        get() {
+            if (field < 0 || field > 250)
+                return 160
+            else
+                return field
+        }
+        set(value) {
+            field = 5 + value
+        }
+
     init {
         //构造函数的函数体
     }
