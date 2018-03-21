00e41984caae863a562b83dbda86ef9783f10bb3
duhongliu <duhongliu@mafengwo.com>
2018-03-21 20:27:35 +0800

    如果觉得某些属性因为一些原因不能对别人可见，就把它定义为private

--- app/src/main/java/com/example/liuhong/weatherkotlintest/net/RequestForecastCommand.kt
+++ app/src/main/java/com/example/liuhong/weatherkotlintest/net/RequestForecastCommand.kt
@@ -8,8 +8,9 @@ import com.example.liuhong.weatherkotlintest.domain.model.ForecastList
  * Created by du on 2018/3/20.
  * Description:
  */
-class RequestForecastCommand(val zipCode: String) : Command<ForecastList> {
-
+class RequestForecastCommand(private val zipCode: String) : Command<ForecastList> {
+    //zipCode只能去得到，不能去修改它
+    //如果觉得某些属性因为一些原因不能对别人可见，就把它定义为private
     override fun execute(): ForecastList {
         val forecastRequest = ForecastRequest(zipCode)
         return ForecastDataMapper().convertFromDataModel(forecastRequest.execute())
